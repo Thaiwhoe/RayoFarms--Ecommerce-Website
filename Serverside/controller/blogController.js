e09bd795b861b9c6a3e830ea67958668cafa0f39
Thaiwhoe <joe4uji@gmail.com>
2023-06-24 19:26:00 +0100

    Added dislike function and solved some minor bugs

--- Serverside/controller/blogController.js
+++ Serverside/controller/blogController.js
@@ -15,20 +15,6 @@ const createBlog = asyncHandler(async(req, res)=> {
     }
 })
 
-const updateBlog = asyncHandler(async(req, res)=> {
-    const { id }= req.params
-    validateMongoDbID(id)
-    try{
-        const updatedBlog = await Blog.findByIdAndUpdate(id, req.body, {
-            new: true
-        });
-        res.json(updatedBlog)
-    }
-    catch(error){
-        throw new Error(error)
-    }
-})
-
 const getAllBlogs = asyncHandler(async(req, res)=> {
     try{
         const getBlogs = await Blog.find()
@@ -48,19 +34,6 @@ const getBlog = asyncHandler(async(req, res)=> {
             $inc: {numOfViews: 1}
         },{ new: true})
         res.json(getBlog)
-    }
-    catch(error){
-        throw new Error(error)
-    }
-})
-
-
-const deleteBlog = asyncHandler(async(req, res)=> {
-    const { id } = req.params;
-    validateMongoDbID(id)
-    try{
-    const deletedBlog = await Blog.findOneAndDelete(id)
-    res.json(deletedBlog)
     }
     catch(error){
         throw new Error(error)
@@ -87,9 +60,8 @@ const likeBlogPost = asyncHandler(async(req, res)=> {
             isDisliked: false,
         }, { new: true });
         res.json(blog)
-        console.log("alreadyLiked ran")
     }
-    console.log(alreadyDisliked)
+
     //this would check for the user in the likes Array in the model(i.e if user has liked the post)
     //then pull the ID from the likes array and set isLiked in the model to false
     //(user would neither like nor dislike blogpost)
@@ -99,7 +71,6 @@ const likeBlogPost = asyncHandler(async(req, res)=> {
             isLiked: false,
         }, { new: true });
         res.json(blog)
-        console.log("isLiked ran")
     } 
 
     //this would would push the user ID to the likes Array in the model making the user like the post
@@ -109,8 +80,77 @@ const likeBlogPost = asyncHandler(async(req, res)=> {
             isLiked: true,
         }, { new: true });
         res.json(blog)
-        console.log("else ran")
     }
 });
 
-module.exports = { createBlog, updateBlog, getAllBlogs, getBlog, deleteBlog, likeBlogPost }
+const disLikeBlogPost = asyncHandler(async(req, res)=> {
+    const { blogId } = req.body;
+    validateMongoDbID(blogId)
+
+    //find the blog that would be liked
+    const blog = await Blog.findById(blogId);
+    //make sure user is Logged in
+    const loginUserId = req?.user?._id;
+    //Check if user has liked the post
+    const isDisliked = blog?.isDisliked
+    //Check if user has disliked the post
+    const alreadyliked = blog?.likes?.find((userId) => userId?.toString() === loginUserId?.toString())
+    //this would check for the user in likes array in the model (i.e if the user has liked the post)
+    //then pull the ID from the like array and set isliked in the Model to false 
+    if (alreadyliked){
+        const blog = await Blog.findByIdAndUpdate(blogId, {
+            $pull: { likes: loginUserId },
+            isLiked: false,
+        }, { new: true });
+        res.json(blog)
+    }
+   
+    //this would check for the user in the likes Array in the model(i.e if user has liked the post)
+    //then pull the ID from the likes array and set isLiked in the model to false
+    //(user would neither like nor dislike blogpost)
+    if (isDisliked){
+        const blog = await Blog.findByIdAndUpdate(blogId, {
+            $pull: { dislikes: loginUserId },
+            isDisliked: false,
+        }, { new: true });
+        res.json(blog)
+    } 
+
+    //this would would push the user ID to the likes Array in the model making the user like the post
+    else{
+        const blog = await Blog.findByIdAndUpdate(blogId, {
+            $push: { dislikes: loginUserId },
+            isDisliked: true,
+        }, { new: true });
+        res.json(blog)
+    }
+});
+
+
+const updateBlog = asyncHandler(async(req, res)=> {
+    const { id }= req.params
+    validateMongoDbID(id)
+    try{
+        const updatedBlog = await Blog.findByIdAndUpdate(id, req.body, {
+            new: true
+        });
+        res.json(updatedBlog)
+    }
+    catch(error){
+        throw new Error(error)
+    }
+})
+
+const deleteBlog = asyncHandler(async(req, res)=> {
+    const { id } = req.params;
+    validateMongoDbID(id)
+    try{
+    const deletedBlog = await Blog.findOneAndDelete(id)
+    res.json(deletedBlog)
+    }
+    catch(error){
+        throw new Error(error)
+    }
+})
+
+module.exports = { createBlog, updateBlog, getAllBlogs, getBlog, deleteBlog, likeBlogPost, disLikeBlogPost }
--- Serverside/routes/blogRoutes.js
+++ Serverside/routes/blogRoutes.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const { authMiddleware, isAdmin } = require('../middlewares/authMiddleWare');
-const { createBlog, updateBlog, getAllBlogs, getBlog, deleteBlog, likeBlogPost } = require('../controller/blogController');
+const { createBlog, updateBlog, getAllBlogs, getBlog, deleteBlog, likeBlogPost, disLikeBlogPost } = require('../controller/blogController');
 const router = express.Router();
 
 
 router.post("/", authMiddleware, isAdmin, createBlog);
-router.put('/likes', authMiddleware, likeBlogPost)
-router.put('/:id',authMiddleware, isAdmin, updateBlog)
-router.get('/', getAllBlogs)
-router.get('/:id', getBlog)
-router.delete('/:id',authMiddleware, isAdmin, deleteBlog)
+router.put('/likes', authMiddleware, likeBlogPost);
+router.put('/dislike',authMiddleware, disLikeBlogPost)
+
+
+router.put('/:id',authMiddleware, isAdmin, updateBlog);
+
+
+router.get('/', getAllBlogs);
+router.get('/:id', getBlog);
+router.delete('/:id',authMiddleware, isAdmin, deleteBlog);
 
 
 
